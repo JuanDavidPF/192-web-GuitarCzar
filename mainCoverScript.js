7a9f622e70d4ec1cedb96987cc79b8fc01dd3255
JuanDavidPF <JuanFavidPF@gmail.com>
2019-09-07 03:19:31 -0500

    added slides show to the cover with more animations, and a bullet control

---
+++ mainCoverScript.js
@@ -0,0 +1,109 @@
+var mainCover = document.querySelector('.mainHeader__cover');
+var bulletCover0 = document.querySelector('#firstBullet');
+var bulletCover1 = document.querySelector('#secondBullet');
+var bulletCover2 = document.querySelector('#thirdBullet');
+var bulletCover3 = document.querySelector('#fourthBullet');
+var bulletCover4 = document.querySelector('#fifthBullet');
+var fondoX = 10;
+var draw = setInterval(draw, 17);
+var counterSlider = setInterval(hiloCoverSlideShow, 10000);
+var counterCover = 0;
+
+
+function hiloCoverSlideShow() {
+    counterCover++;
+    if (counterCover == 5) counterCover = 0;
+
+
+    if (fondoX == 10) {
+        fondoX = 55;
+
+    } else if (fondoX == 55) {
+        fondoX = 10;
+    }
+}
+
+
+
+
+function draw() {
+    console.log(fondoX);
+    mainCover.style.backgroundPosition = fondoX + '%';
+
+    switch (counterCover) {
+        case 0:
+            mainCover.style.backgroundImage = 'url("./data/mainCover0.png")';
+            bulletCover0.checked = true;
+
+            break;
+
+        case 1:
+            mainCover.style.backgroundImage = 'url("./data/mainCover1.png")';
+            bulletCover1.checked = true;
+            break;
+
+        case 2:
+            mainCover.style.backgroundImage = 'url("./data/mainCover2.png")';
+            bulletCover2.checked = true;
+
+            break;
+
+        case 3:
+            mainCover.style.backgroundImage = 'url("./data/mainCover3.png")';
+            bulletCover3.checked = true;
+
+            break;
+
+        case 4:
+            mainCover.style.backgroundImage = 'url("./data/mainCover4.png")';
+            bulletCover4.checked = true;
+
+            break;
+
+    }
+
+}
+
+
+
+
+//passing cover images with the cover bullets
+
+bulletCover0.addEventListener("click", function () {
+    counterCover = 0;
+    clearInterval(counterSlider);
+    counterSlider = setInterval(hiloCoverSlideShow, 7000);
+    fondoX = 5;
+
+});
+
+bulletCover1.addEventListener("click", function () {
+    counterCover = 1;
+    clearInterval(counterSlider);
+    counterSlider = setInterval(hiloCoverSlideShow, 7000);
+    fondoX = 55;
+
+});
+
+bulletCover2.addEventListener("click", function () {
+    counterCover = 2;
+    clearInterval(counterSlider);
+    counterSlider = setInterval(hiloCoverSlideShow, 7000);
+    fondoX = 5;
+
+});
+
+bulletCover3.addEventListener("click", function () {
+    counterCover = 3;
+    clearInterval(counterSlider);
+    counterSlider = setInterval(hiloCoverSlideShow, 7000);
+    fondoX = 55;
+
+});
+bulletCover4.addEventListener("click", function () {
+    counterCover = 4;
+    clearInterval(counterSlider);
+    counterSlider = setInterval(hiloCoverSlideShow, 7000);
+    fondoX = 5;
+
+});
